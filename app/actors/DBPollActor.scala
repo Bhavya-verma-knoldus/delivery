4db54f94cfe6930361b486e7820706522bce48c8
Akhiltrivedi83 <Akhiltrivedi83@gmail.com>
2023-11-22 12:40:15 +0530

    Actor-db-polling-delivery

--- app/actors/DBPollActor.scala
+++ app/actors/DBPollActor.scala
@@ -1,17 +1,23 @@
 package actors
 
-import anorm.{RowParser, SQL, Sql, SqlParser, ~}
+import anorm._
+import com.nashtech.delivery.v1.anorm.conversions.Standard.columnToJsValue
 import org.joda.time.DateTime
+import play.api.db.Database
+import play.api.libs.json.{JsValue, Json}
 
-import scala.util.{Failure, Success, Try}
+import scala.util.{Failure, Random, Success, Try}
 
 abstract class DBPollActor(schema: String = "public", table: String) extends PollActor {
-  val processingTable = {
+
+  def db: Database
+
+  private val processingTable = {
     val tempName = s"${table}_processing_queue"
-    if(schema.equalsIgnoreCase("public")) tempName
+    if (schema.equalsIgnoreCase("public")) tempName
     else s"${schema}.${tempName}"
   }
-  val journalTable = {
+  private val journalTable = {
     val tempName = s"${table}_journal"
     if (schema.equalsIgnoreCase("public")) tempName
     else s"${schema}.${tempName}"
@@ -24,7 +30,7 @@ abstract class DBPollActor(schema: String = "public", table: String) extends Pol
     println("[DBPollActor] Pre-Start")
   }
 
-  def process(record: ProcessQueueOrder): Try[Unit]
+  def process(record: ProcessQueueDelivery): Try[Unit]
 
   override def processRecord(): Unit = {
     log.info("Inside processRecord method")
@@ -33,10 +39,10 @@ abstract class DBPollActor(schema: String = "public", table: String) extends Pol
 
   }
 
-  def safeProcessRecord(record: ProcessQueueOrder) = {
+  private def safeProcessRecord(record: ProcessQueueDelivery) = {
     Try {
       log.info("Inside safeProcessRecord method")
-      println("nside safeProcessRecord method")
+      println("Inside safeProcessRecord method")
       process(record)
     } match {
       case Success(_) =>
@@ -51,65 +57,122 @@ abstract class DBPollActor(schema: String = "public", table: String) extends Pol
     }
   }
 
-  private def deleteProcessingQueueRecord(id: String) = {
-    // TODO: Integrate with Database
-    DeleteQuery(id)
+  private def deleteProcessingQueueRecord(id: Int) = {
+    db.withConnection { implicit connection =>
+      SQL(deleteQuery(id)).executeUpdate()
+    }
   }
 
-  private def insertJournalRecord(record: ProcessQueueOrder) = {
-    // TODO: Integrate with Database
-    //database.withConnection(c: Connection => SQL(InsertQuery(record)).executeInsert())
-    // SQL(InsertQuery(record)).executeInsert()
+  private def insertJournalRecord(record: ProcessQueueDelivery) = {
+    db.withConnection { implicit connection =>
+      insertQuery(record).executeInsert()
+    }
   }
 
-  private def setErrors(processingQueueId: String, throwable: Throwable) = {
-    // TODO: Integrate with Database
-    // SQL(SetErrorsQuery(processingQueueId, throwable)).executeUpdate()
-  }
-  private def getEarliestRecord(processingTable: String): ProcessQueueOrder = {
-    // TODO: Integrate with database
-    // SQL(BaseQuery).as(ProcessingQueueOrderParser().single)
-    ProcessQueueOrder(processingQueueId = "1", id = "1", number = "1", merchantId = "1", submittedAt = DateTime.now(), total = 234.43, operation = "Insert")
+  private def setErrors(processingQueueId: Int, throwable: Throwable) = {
+    db.withConnection { implicit connection =>
+      SQL(setErrorsQuery(processingQueueId, throwable)).executeUpdate()
+    }
   }
 
-  private def BaseQuery =
+  private def getEarliestRecord(processingTable: String): ProcessQueueDelivery = {
+    db.withConnection { implicit connection =>
+      SQL(baseQuery).as(processingQueueDeliveryParser().single)
+    }
+  }
+
+  private def baseQuery =
     s"""
-       |select processing_queue_id, id, number, merchant_id, submitted_at, total, operation
+       |select processing_queue_id, id, order_number, merchant_id, estimated_delivery_date, origin, destination, contact_info, created_at,
+       |updated_at,operation
        |from ${processingTable}
-       |order by created_at asc
+       |order by created_at asc limit 1
        |""".stripMargin
 
-  private def DeleteQuery(id: String) =
+  private def deleteQuery(id: Int) =
     s"""
        |delete from ${processingTable} where processing_queue_id = $id
        |""".stripMargin
 
-  private def InsertQuery(record: ProcessQueueOrder) =
-    s"""
-       |insert into $journalTable (number, merchant_id, submitted_at, total, operation)
-       |values (${record.number}, ${record.merchantId}, ${record.submittedAt}, ${record.submittedAt}, ${record.total}, ${record.operation})
-       |""".stripMargin
+  private def insertQuery(record: ProcessQueueDelivery) = {
+    SQL(
+      s"""
+         |INSERT INTO $journalTable (journal_id, processing_queue_id, id, order_number, merchant_id, estimated_delivery_date,
+         | origin, destination, contact_info, created_at,
+         |updated_at, journal_timestamp, journal_operation
+         |)
+         |VALUES
+         |(
+         |{journal_id},
+         |{processing_queue_id},
+         |{id},
+         |{order_number},
+         |{merchant_id},
+         |{estimated_delivery_date}::date,
+         |{origin}::jsonb,
+         |{destination}::jsonb,
+         |{contact_info}::jsonb,
+         |{created_at}::date,
+         |{updated_at}::date,
+         |{journal_timestamp}::date,
+         |{journal_operation}
+         |)
+         |returning *
+         |""".stripMargin)
+      .on("journal_id" -> Random.nextLong(1000))
+      .on("processing_queue_id" -> record.processingQueueId)
+      .on("id" -> record.id)
+      .on("order_number" -> record.orderNumber)
+      .on("merchant_id" -> record.merchantId)
+      .on("estimated_delivery_date" -> record.estimateDeliveryDate.toString)
+      .on("origin" -> Json.toJson(record.origin).toString())
+      .on("destination" -> Json.toJson(record.destination).toString())
+      .on("contact_info" -> Json.toJson(record.contactInfo).toString())
+      .on("created_at" -> record.createdAt.toString)
+      .on("updated_at" -> record.updatedAt.toString)
+      .on("journal_timestamp" -> DateTime.now().toString)
+      .on("journal_operation" -> record.operation)
+  }
 
-  private def SetErrorsQuery(id: String, ex: Throwable) = {
+  private def setErrorsQuery(id: Int, ex: Throwable) = {
     s"""
-       |update $processingTable set error = ${ex.getMessage} where processing_queue_id = $id
+       |update $processingTable set error_message = '${ex.getMessage}'
+       | where processing_queue_id = $id
        |""".stripMargin
   }
-  private def ProcessingQueueOrderParser(): RowParser[ProcessQueueOrder] = {
-    SqlParser.str("processing_queue_id") ~
+
+  private def processingQueueDeliveryParser(): RowParser[ProcessQueueDelivery] = {
+    SqlParser.int("processing_queue_id") ~
       SqlParser.str("id") ~
-      SqlParser.str("number") ~
+      SqlParser.str("order_number") ~
       SqlParser.str("merchant_id") ~
-      SqlParser.get[DateTime]("submitted_at") ~
-      SqlParser.double("total") ~
+      SqlParser.get[DateTime]("estimated_delivery_date") ~
+      SqlParser.get[JsValue]("origin") ~
+      SqlParser.get[JsValue]("destination") ~
+      SqlParser.get[JsValue]("contact_info") ~
+      SqlParser.get[DateTime]("created_at") ~
+      SqlParser.get[DateTime]("updated_at") ~
       SqlParser.str("operation") map {
 
-      case processingQueueId ~ id ~ number ~ merchantId ~ submittedAt ~ total ~ operation =>
-        ProcessQueueOrder(
-          processingQueueId , id , number , merchantId , submittedAt, total , operation
+      case processingQueueId ~ id ~ orderNumber ~ merchantId ~ estimateDeliveryDate ~ origin ~ destination ~ contactInfo ~ createdAt ~ submittedAt ~ operation =>
+        ProcessQueueDelivery(
+          processingQueueId, id, orderNumber, merchantId, estimateDeliveryDate, origin, destination, contactInfo, createdAt, submittedAt, operation
         )
     }
   }
 }
+case class ProcessQueueDelivery(
+                                 processingQueueId: Int,
+                                 id: String,
+                                 orderNumber: String,
+                                 merchantId: String,
+                                 estimateDeliveryDate: DateTime,
+                                 origin: JsValue,
+                                 destination: JsValue,
+                                 contactInfo: JsValue,
+                                 createdAt: DateTime,
+                                 updatedAt: DateTime,
+                                 operation: String
+                               )
 
-case class ProcessQueueOrder(processingQueueId: String, id: String, number: String, merchantId: String, submittedAt: DateTime, total: Double, operation: String)
+
